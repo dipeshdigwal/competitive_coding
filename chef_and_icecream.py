e9ed163637d8e9e9529d1520515a57b79ca14746
dipesh digwal <dipeshdigwal1@icloud.com>
2020-06-22 15:50:27 +0530

    chef_and_icecream

---
+++ chef_and_icecream.py
@@ -0,0 +1,24 @@
+def can_icecream_be_served(A):
+    shops_money = [0]*4  # 0:Rs 5 , 1:Rs 10 , 2:Rs 15 , 3:Total
+    for i in A:
+        balance = int(i)-5
+        if(balance == 0):
+            shops_money[0] += 1
+        elif(balance == 5):
+            if(shops_money[0] < 1):
+                return("NO")
+            else:
+                shops_money[0] -= 1
+        else:
+            if(shops_money[0] < 2 and shops_money[1] < 1):
+                return("NO")
+            else:
+                shops_money[0] -= 2
+    return("YES")
+
+
+T = int(input())
+for i in range(T):
+    N = int(input())
+    A = input().split(" ")
+    print(can_icecream_be_served(A))
