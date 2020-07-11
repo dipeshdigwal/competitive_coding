e8525985db6f101f9534aa17fd62da91050de924
dipesh digwal <dipeshdigwal1@icloud.com>
2020-07-11 20:57:31 +0530

    chef_and_dragon_dens2

--- chef_and_dragon_dens.py
+++ chef_and_dragon_dens.py
@@ -1,62 +1,64 @@
-def slope(n0,n1):
+def slope(n0, n1):
     return((int(H[n0-1])-int(H[n1-1]))/(n0-n1))
 
-def maxGlide2(start, stop, flag):
-    global H2
-    global ans
-    global maxi
-    if(start == stop):
-        y = sum(H2)
-        if(y > maxi):
-            maxi = y
-            ans.append(list(H2))
-    elif(start < stop):
-        maxSlope=-10e10
-        if(flag == 1):
-            H2 = []
-            maxi = 0
-            ans = []
-            H2.append(int(A[start-1]))
-        for i in range(start+1, stop+1):
-            s2=slope(start,i)
-            if(int(H[start-1]) > int(H[i-1])):
-                if(s2 >=maxSlope):
-                    maxSlope=s2
-                    H2.append(int(A[i-1]))
-                    maxGlide2(i, stop, 0)
-                    H2.pop()
-            else:
-                break
-        if(flag == 1):
-            H2.pop()
-    else:
-        minSlope=10e10
-        if(flag == 1):
-            H2 = []
-            maxi = 0
-            ans = []
-            H2.append(int(A[start-1]))
-        for i in range(start-1, stop-1, -1):
-            s2=slope(start,i)
-            if(int(H[start-1]) > int(H[i-1])):
-                if(s2<=minSlope):
-                    minSlope=s2
-                    H2.append(int(A[i-1]))
-                    maxGlide2(i, stop, 0)
-                    H2.pop()
-            else:
-                break
-        if(flag == 1):
-            H2.pop()
+
+def maxGlide(start, stop):
+    stringIndex = str(start)+","+str(stop)
+    try:
+        return(mem[stringIndex])
+    except KeyError:
+        if(start == stop):
+            return(int(A[start-1]))
+        elif(start < stop):
+            maxGlideVal = 0
+            sumGlide = int(A[start-1])
+            maxSlope = -10e10
+            for i in range(start+1, stop+1):
+                s2 = slope(start, i)
+                if(int(H[start-1]) > int(H[i-1])):
+                    if(s2 >= maxSlope):
+                        maxSlope = s2
+                        x = maxGlide(i, stop)
+                        if(x != -1):
+                            sumGlide += x
+                            if(sumGlide > maxGlideVal):
+                                maxGlideVal = sumGlide
+                else:
+                    sumGlide = -1
+                    break
+
+        else:
+            maxGlideVal = 0
+            sumGlide = int(A[start-1])
+            minSlope = 10e10
+            for i in range(start-1, stop-1, -1):
+                s2 = slope(start, i)
+                if(int(H[start-1]) > int(H[i-1])):
+                    if(s2 <= minSlope):
+                        minSlope = s2
+                        x = maxGlide(i, stop)
+                        if(x != -1):
+                            sumGlide += x
+                            if(sumGlide > maxGlideVal):
+                                maxGlideVal = sumGlide
+                else:
+                    sumGlide = -1
+                    break
+
+        if(sumGlide == -1):
+            mem[stringIndex] = -1
+            return(-1)
+        mem[stringIndex] = maxGlideVal
+        return(maxGlideVal)
 
 
 def updater(b, c):
+    global mem
     A[b-1] = str(c)
+    mem = {}
 
 
-H2 = []
-maxi = 0
-ans = []
+mem = {}
 w = input().split(" ")
 N = int(w[0])
 Q = int(w[1])
@@ -67,8 +69,4 @@ for i in range(Q):
     if(X[0] == "1"):
         updater(int(X[1]), int(X[2]))
     else:
-        maxGlide2(int(X[1]), int(X[2]), 1)
-        if(maxi != 0):
-            print(maxi)
-        else:
-            print(-1)
+        print(maxGlide(int(X[1]), int(X[2])))
