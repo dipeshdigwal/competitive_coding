1925a5f35f472f854df069fdf07b4a4c699e61e1
dipesh digwal <dipeshdigwal1@icloud.com>
2020-07-13 15:26:46 +0530

    JulyCookoff

--- JulyCookoff/chefina_and_swaps.py
+++ JulyCookoff/chefina_and_swaps.py
@@ -10,9 +10,33 @@ def dictCompare(A, B):
     countDelKeys = 0
     for i in A.keys():
         try:
-            B[i] -= 1
-            A[i] -= 1
-            countDelKeys += 1
+            x = minimum(A[i], B[i])
+            B[i] -= x
+            A[i] -= x
+            countDelKeys += x
+            if(A[i] != 0):
+                if(A[i] % 2 == 0):
+                    y1 = N+1
+                    y2 = N+1
+                    y = 0
+                    while(y1 != 0):
+                        for j in B.keys():
+                            if(B[j] >=2):
+                                y1 = int(A[i]/2)
+                                y2 = int(B[j]/2)
+                                y = minimum(y1, y2)
+                                totalCost += y*minimum(int(i), int(j))
+                                A[i] -= 2*y
+                                B[j] -= 2*y
+                                countDelKeys += 2*y
+                                y1 -= y
+                                y2 -= y
+                                if(y1 == 0):
+                                    break
+                        else:
+                            return(-1)
+                else:
+                    return(-1)
         except KeyError:
             if(A[i] % 2 == 0 and A[i] > 0):
                 y1 = N
@@ -20,7 +44,7 @@ def dictCompare(A, B):
                 y = 0
                 while(y1 != 0):
                     for j in B.keys():
-                        if(B[j] % 2 == 0 and B[j] > 0):
+                        if(B[j] >=2):
                             y1 = int(A[i]/2)
                             y2 = int(B[j]/2)
                             y = minimum(y1, y2)
